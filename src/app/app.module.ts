006cf2fd3888569e1a0232784813d77fac98eda4
Lionel <Lionel.Delsupexhe@gmail.com>
2022-02-21 10:38:47 +0100

    split layout in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SolutionComponent } from './components/solution/solution.component';
+import { EncoderComponent } from './components/encoder/encoder.component';
+import { AttemptsPanelComponent } from './components/attempts-panel/attempts-panel.component';
+import { AttemptComponent } from './components/attempts-panel/attempt/attempt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolutionComponent,
+    EncoderComponent,
+    AttemptsPanelComponent,
+    AttemptComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/attempts-panel/attempts-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attempts-panel',
+  templateUrl: './attempts-panel.component.html',
+  styleUrls: ['./attempts-panel.component.css']
+})
+export class AttemptsPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
